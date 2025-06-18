30b58c6182aa967e747aec6d0d848c262d445d7d
Ayele Masresha <weletesadok@gmail.com>
2025-06-18 05:41:05 -0400

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,7 +41,7 @@ module.exports = {
       const company = await prisma.organization.findFirst({
         where: {
           identification: {
-            contains: `"shortname":"${orgShortCode}"`,
+            shortname: orgShortCode,
           },
         },
       });
